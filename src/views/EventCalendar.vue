d4e19646f9bc3da56ec975dd58eb56e6ed68b5e9
iko95 <iko95@gmx.de>
2020-01-04 18:57:47 +0100

    changed view to home2 and created eventcalendar view

---
+++ src/views/EventCalendar.vue
@@ -0,0 +1,28 @@
+<template>
+   <div id="pageview">
+      <h1>Event Calendar for Innovation and Tech Events in Leipzig</h1>
+      <div id="cards">
+         <div class="header">
+            <p>Startup Stammtisch Leipzig</p>
+         </div>
+         <div class="time">
+            <p>7 pm</p>
+         </div>
+         <div class="location">
+            <p>Basislager Coworking</p>
+         </div>
+         <div class="card-image">
+            <img src="https://uploads-ssl.webflow.com/58f76c68b8bb6a16cafdc9c3/5d4c355bd1af162548365162_DSC07446.JPG" alt="picture">
+         </div>
+      </div>
+   </div>
+</template>
+
+
+<style scoped lang="scss">
+
+#cards{
+   
+}
+
+</style>
